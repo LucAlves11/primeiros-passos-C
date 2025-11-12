f5b7bc46bd8b7a456efc875415d7db3267acf15c
LucAlves11 <luccalves1969@gmail.com>
2025-11-12 09:45:42 -0300

    exemplos variaveis

---
+++ variaveis/variaveis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;        //numero inteiro
+    int quantidade = 1;    //numero inteiro
+    float altura = 1.75;   //numero decimal
+    double peso = 80.3;    //mais casas decimais, numeros maiores
+    char letra = 'A';      //letra
+    char nome[20];         //nome com 20 caracteres
+     
+}
+
+/*
+Nomes de variáveis devem começar com uma letra (a-z, A-Z) ou um sublinhado (_). Eles não podem começar com um número. 
+
+int idade;      // Correto 
+int altura;     // Correto 
+int 1salario;   // Incorreto 
+*/ 
+
